3a4df9043aa6c4113d75efedf670faef4a017b54
Dominic <94198408+its-niceferrari@users.noreply.github.com>
2023-06-23 19:13:52 -0500

    Improved usability and comments

--- batterybuddy.py
+++ batterybuddy.py
@@ -2,11 +2,17 @@ import psutil
 import time
 import tkinter as tk
 
+CUSTOM_PERCENTAGE = 30  # Can be adjusted to display alerts at different battery percentages
+CUSTOM_SLEEP = 5  # Can be adjusted to shorten or lengthen the amount of minutes between alerts
+
 def check_battery():
+    # Defines variables for the battery percentage and charging status
     battery = psutil.sensors_battery()
     percent = battery.percent
     charging = battery.power_plugged
-    if percent < 30 and not charging:
+
+    # Displays an alert if the battery percentage is less than a user-defined value
+    if percent < CUSTOM_PERCENTAGE and not charging:
         alert_window = tk.Tk()
         alert_window.title("Battery Alert!")
         alert_window.geometry("500x500")
@@ -16,4 +22,4 @@ def check_battery():
 
 while True:
     check_battery()
-    time.sleep(300)  # 5 minutes in seconds
+    time.sleep(CUSTOM_SLEEP * 60)  # Converts the user-defined sleep time into seconds
